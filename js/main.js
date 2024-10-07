54f4b7041450fe2f39f1c4d4fc7a1243d7170765
maezoo <aa00456@naver.com>
2024-10-07 21:42:57 +0900

    structure main layout and add slide in first row

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// ===========================
+// =================
 //푸터 패밀리 =========
 const plusBtn = document.querySelector('.select');
 const selectBox = document.querySelector('.select_wrap');
@@ -6,10 +6,7 @@ const verticalStick = document.querySelector('.ver_rod');
 
 function familySite() {
     plusBtn.addEventListener('click', function () {
-        // selectBox의 'show' 클래스 토글
-        const stickVisible = selectBox.classList.toggle('show'); // show 클래스를 토글하고 그 결과를 isVisible에 저장
-
-        // isVisible이 true일 경우 verticalStick 숨기기, false일 경우 보이기
+        const stickVisible = selectBox.classList.toggle('show');
         verticalStick.style.display = stickVisible ? 'none' : 'block';
     });
 }
---
+++ js/slick.js
@@ -0,0 +1,8 @@
+
+$('.slick-wrapper').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+});
